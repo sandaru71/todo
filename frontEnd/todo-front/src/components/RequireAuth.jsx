a2e70ced3249ec6dd23d362731a8f6294682c9b0
Sandaru Senanayake <ssandarus71@gmail.com>
2024-07-12 16:41:52 +0530

    ..

--- frontEnd/todo-front/src/components/RequireAuth.jsx
+++ frontEnd/todo-front/src/components/RequireAuth.jsx
@@ -2,17 +2,6 @@ import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/UseAuth";
 
 export default function RequireAuth() {
-  // const { auth } = useAuth();
-  // const location = useLocation();
-
-  // console.log("require auth", auth);
-
-  // return auth?.token ? (
-  //   <Outlet />
-  // ) : (
-  //   <Navigate to="/signup" state={{ from: location }} replace />
-  // );
-
   const { auth } = useAuth();
 
   if (auth?.token) {
